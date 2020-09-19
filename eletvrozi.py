b1bec3f1c4ae016bd04c890cea119bfa76f8fda3
Borna Gojšić <69301624+bornagojsic@users.noreply.github.com>
2020-09-19 11:44:34 +0200

    Update eletvrozi.py

--- eletvrozi.py
+++ eletvrozi.py
@@ -1,6 +1,7 @@
 def main():
-	n = len(s := input('Upisi string: ')) // 2
-	print(s[:n][::-1] + s[n] + s[n + 1:][::-1] if len(s) % 2 else s[:n][::-1] + s[n:][::-1])
+	s = input('Upisi string: ')
+	if s:
+		print(s[:(n := len(s) // 2)][::-1] + s[n] + s[n + 1:][::-1] if len(s) % 2 else s[:n][::-1] + s[n:][::-1])
 
 
 if __name__ == '__main__':
