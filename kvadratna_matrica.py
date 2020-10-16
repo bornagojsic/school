1058f277f06bd952a170685423bcec54bece6335
Borna Gojšić <69301624+bornagojsic@users.noreply.github.com>
2020-10-16 17:13:47 +0200

    Create kvadratna_matrica.py

---
+++ kvadratna_matrica.py
@@ -0,0 +1,8 @@
+def main():
+	## broj redaka
+	n = int(input("Upisi broj n: "))
+	## tablica
+	t = [list(map(int, input(f"Upisi {i + 1}. redak: ").split())) for i in range(n)]
+
+if __name__ == '__main__':
+	main()
