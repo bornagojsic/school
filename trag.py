25c66e357581ecb571141e7383f7b7626a826efa
Borna Gojšić <69301624+bornagojsic@users.noreply.github.com>
2020-10-20 07:56:34 +0200

    Update trag.py

--- trag.py
+++ trag.py
@@ -3,7 +3,7 @@ def main():
 	n = int(input("Upisi broj n: "))
 	## tablica
 	t = [list(map(int, input(f"Upisi {i + 1}. redak: ").split())) for i in range(n)]
-	print(f"Trag matrice je {sum([t[i][j] for i in range(n) for j in range(n) if i == j])}")
+	print(f"Trag matrice je {sum([t[i][i] for i in range(n)])}")
 
 if __name__ == '__main__':
 	main()
