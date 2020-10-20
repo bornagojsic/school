9c1dc84691353bec19ea3de90c3ca82ba31733f6
Borna Gojšić <69301624+bornagojsic@users.noreply.github.com>
2020-10-20 07:56:28 +0200

    Create transponirana.py

---
+++ transponirana.py
@@ -0,0 +1,17 @@
+def main():
+	## broj redaka
+	r = int(input("Upisi broj redaka: "))
+	## broj stupaca
+	s = int(input("Upisi broj stupaca: "))
+	## tablica
+	t = [list(map(int, input(f"Upisi {i + 1}. redak: ").split())) for i in range(r)]
+	print("Ispis matrice:")
+	[print(r) for r in t]
+	t2 = [[t[j][i] for j in range(len(t))] for i in range(len(t[0]))] 
+	print("Ispis transponirane matrice:")
+	[print(r) for r in t2]
+
+
+
+if __name__ == '__main__':
+	main()
