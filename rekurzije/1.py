d04df6ad6334c071915e745a002474713d20261b
Borna Gojšić <69301624+bornagojsic@users.noreply.github.com>
2020-12-11 15:18:21 +0100

    Create 1.py

---
+++ rekurzije/1.py
@@ -0,0 +1,16 @@
+##          f(x-3) + 3,  x > 3
+##	f(x) =  2x + 1    ,  x = 3   ==>  f(3) = 7
+##          x^2 + 2   ,  x < 3
+
+##  f(9) = f(6) + 3 = (f(3) + 3) + 3 = (7 + 3) + 3 = 10 + 3 = 13
+
+def f(x):
+	if x == 3:
+		return 7
+	elif x > 3:
+		return f(x - 3) + 3
+	return x**2 + 2
+
+print(f"f(9) = {f(9)}")
+
+print(f"f(x) = {f(int(input('Upiši x: ')))}")
