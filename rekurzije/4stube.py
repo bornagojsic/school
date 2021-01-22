121723baefa7dca07fef98bf49fd318fcae90cd4
Borna Gojšić <69301624+bornagojsic@users.noreply.github.com>
2021-01-22 16:13:08 +0100

    Create 4stube.py

---
+++ rekurzije/4stube.py
@@ -0,0 +1,16 @@
+def f(n):
+	'''
+	if n == 1:
+		return 1
+	if n == 2:
+		return 2
+	if n == 3:
+		return 4
+	if n == 4:
+		return 8
+	'''
+	if n in [1,2,3,4]:
+		return 2**(n-1)
+	return f(n-1) + f(n-2) + f(n-3) + f(n-4)
+
+print(f"REZULTAT: { f( int( input( 'UPISI N: ' ) ) ) }")
